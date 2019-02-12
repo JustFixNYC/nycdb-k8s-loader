8c1d44827fcee354822452e2483f96efad455056
Atul Varma <varmaa@gmail.com>
2019-02-12 07:16:00 -0500

    Load dependee datasets twice in wowutil test.

--- tests/test_wowutil.py
+++ tests/test_wowutil.py
@@ -1,18 +1,16 @@
 import psycopg2
-from nycdb.dataset import Dataset
 
 from .conftest import DATABASE_URL
-from load_dataset import Config
+from load_dataset import Config, load_dataset
 import wowutil
 
 
-def test_it_works(db, slack_outbox):
-    config = Config(database_url=DATABASE_URL, use_test_data=True)
+def load_dependee_datasets(config: Config):
     for dataset_name in wowutil.WOW_YML['dependencies']:
-        ds = Dataset(dataset_name, args=config.nycdb_args)
-        ds.db_import()
-    wowutil.main(['build'], db_url=DATABASE_URL)
+        load_dataset(dataset_name, config)
 
+
+def ensure_wow_works():
     with psycopg2.connect(DATABASE_URL) as conn:
         with conn.cursor() as cur:
             cur.execute("SELECT COUNT(*) FROM wow.wow_bldgs")
@@ -22,8 +20,21 @@ def test_it_works(db, slack_outbox):
             cur.execute("SET search_path TO wow, public")
             cur.execute("SELECT wow.get_assoc_addrs_from_bbl('blah')")
 
-    assert slack_outbox[0] == 'Rebuilding Who Owns What tables...'
-    assert slack_outbox[1] == 'Finished rebuilding Who Owns What tables.'
+
+def test_it_works(db, slack_outbox):
+    config = Config(database_url=DATABASE_URL, use_test_data=True)
+    load_dependee_datasets(config)
+    wowutil.main(['build'], db_url=DATABASE_URL)
+
+    ensure_wow_works()
+    assert slack_outbox[-2] == 'Rebuilding Who Owns What tables...'
+    assert slack_outbox[-1] == 'Finished rebuilding Who Owns What tables.'
+
+    # Ensure that reloading the dependee datasets doesn't raise
+    # an exception.
+    load_dependee_datasets(config)
 
     # Ensure runing build again doesn't raise an exception.
     wowutil.main(['build'], db_url=DATABASE_URL)
+
+    ensure_wow_works()
