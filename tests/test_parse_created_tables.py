5fe68a3cc7eb540a523a38143cac1f3d27af07f4
Atul Varma <varmaa@gmail.com>
2020-05-22 10:43:53 -0400

    Support CREATE TABLE statements w/ IdentifierLists in them. (#48)
    It looks like #47 is failing because the code we use to parse SQL files to detect what tables are being created by them isn't accounting for `IdentifierList` tokens from the `sqlparse` library, which apparently the SQL for WoW's new `create_bldgs_table` uses.  This adds support for them and makes sure that both WoW's new and old `create_bldgs_table` parse as expected.

--- tests/test_parse_created_tables.py
+++ tests/test_parse_created_tables.py
@@ -1,7 +1,14 @@
+from pathlib import Path
+
+
 from lib.parse_created_tables import (
     parse_created_tables,
     parse_nycdb_created_tables
 )
+
+MY_DIR = Path(__file__).parent.resolve()
+
+SQL_DIR = MY_DIR / 'sql'
 
 
 def test_it_returns_empty_list_when_no_tables_are_created():
@@ -38,6 +45,19 @@ def test_it_parses_create_table_statements_2():
     """
 
     assert parse_created_tables(sql) == ['foo']
+
+
+
+def test_it_parses_old_create_table_statements_in_wow():
+    sql = (SQL_DIR / "wow_old_create_bldgs_table.sql").read_text()
+
+    assert parse_created_tables(sql) == ['wow_bldgs']
+
+
+def test_it_parses_new_create_table_statements_in_wow():
+    sql = (SQL_DIR / "wow_new_create_bldgs_table.sql").read_text()
+
+    assert parse_created_tables(sql) == ['wow_bldgs']
 
 
 def test_it_parses_nycdb_files():
