7ff63f1acf2deb360e8ff814555fc2961200dca7
Maxwell Austensen <maxwell.austensen@nyu.edu>
2022-03-28 20:10:02 +0000

    add black to dev req, dev container, & apply

--- tests/test_scheduling.py
+++ tests/test_scheduling.py
@@ -3,12 +3,14 @@ import pytest
 from scheduling import Schedule
 
 
-
-@pytest.mark.parametrize('value,expected', [
-    (Schedule.DAILY_12AM, "0 4 * * ?"),
-    (Schedule.DAILY_7AM, "0 11 * * ?"),
-    (Schedule.EVERY_OTHER_DAY, "0 4 */2 * ?"),
-    (Schedule.YEARLY, "@yearly"),
-])
+@pytest.mark.parametrize(
+    "value,expected",
+    [
+        (Schedule.DAILY_12AM, "0 4 * * ?"),
+        (Schedule.DAILY_7AM, "0 11 * * ?"),
+        (Schedule.EVERY_OTHER_DAY, "0 4 */2 * ?"),
+        (Schedule.YEARLY, "@yearly"),
+    ],
+)
 def test_k8s_works(value, expected):
     assert value.k8s == expected
