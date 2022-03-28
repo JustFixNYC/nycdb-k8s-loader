0e9f613f276bfecd9fc3f08e13c4fe2bbe775112
Sam Rabiyah <sraby@users.noreply.github.com>
2022-03-28 14:54:43 -0400

    Remove other AWS infrastructure

--- tests/test_scheduling.py
+++ tests/test_scheduling.py
@@ -4,16 +4,6 @@ from scheduling import Schedule
 
 
 @pytest.mark.parametrize('value,expected', [
-    (Schedule.DAILY_12AM, "cron(0 4 * * ? *)"),
-    (Schedule.DAILY_7AM, "cron(0 11 * * ? *)"),
-    (Schedule.EVERY_OTHER_DAY, "cron(0 4 */2 * ? *)"),
-    (Schedule.YEARLY, "rate(365 days)"),
-])
-def test_aws_works(value, expected):
-    assert value.aws == expected
-
-
-@pytest.mark.parametrize('value,expected', [
     (Schedule.DAILY_12AM, "0 4 * * ?"),
     (Schedule.DAILY_7AM, "0 11 * * ?"),
     (Schedule.EVERY_OTHER_DAY, "0 4 */2 * ?"),
