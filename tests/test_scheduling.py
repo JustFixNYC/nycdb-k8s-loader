10ed61fcdffe9bc8af8392f1a3484e992c2b1670
Maxwell Austensen <maxwell.austensen@nyu.edu>
2022-03-28 17:25:18 +0000

    update tests for time change

--- tests/test_scheduling.py
+++ tests/test_scheduling.py
@@ -4,8 +4,8 @@ from scheduling import Schedule
 
 
 @pytest.mark.parametrize('value,expected', [
-    (Schedule.DAILY, "cron(0 5 * * ? *)"),
-    (Schedule.EVERY_OTHER_DAY, "cron(0 5 */2 * ? *)"),
+    (Schedule.DAILY, "cron(0 4 * * ? *)"),
+    (Schedule.EVERY_OTHER_DAY, "cron(0 4 */2 * ? *)"),
     (Schedule.YEARLY, "rate(365 days)"),
 ])
 def test_aws_works(value, expected):
@@ -13,8 +13,8 @@ def test_aws_works(value, expected):
 
 
 @pytest.mark.parametrize('value,expected', [
-    (Schedule.DAILY, "0 5 * * ?"),
-    (Schedule.EVERY_OTHER_DAY, "0 5 */2 * ?"),
+    (Schedule.DAILY, "0 4 * * ?"),
+    (Schedule.EVERY_OTHER_DAY, "0 4 */2 * ?"),
     (Schedule.YEARLY, "@yearly"),
 ])
 def test_k8s_works(value, expected):
