7fd30328bd76edf35e4ae7e5046ec8ad9753830d
Sam Rabiyah <sraby@users.noreply.github.com>
2022-03-28 15:05:15 -0400

    Remove aws_schedule_tasks.py and other AWS infrastructure (#104)
    This removes the aws_schedule_tasks.py file and other AWS infrastructure from our codebase as not to confuse engineers.

--- scheduling.py
+++ scheduling.py
@@ -22,23 +22,6 @@ class Schedule(Enum):
     YEARLY = "@yearly"
 
     @property
-    def aws(self) -> str:
-        """
-        The schedule in terms Amazon Web Services (AWS) expects.
-
-        Note that all times must be specified in UTC.
-
-        For more details on AWS's schedule expressions, see:
-
-        https://docs.aws.amazon.com/AmazonCloudWatch/latest/events/ScheduledEvents.html
-        """
-
-        if self == Schedule.YEARLY:
-            return 'rate(365 days)'
-        # Note the additional "*"; AWS has a sixth field that is the year.
-        return f"cron({self.value} *)"
-
-    @property
     def k8s(self) -> str:
         """
         The schedule in terms Kubernetes CronJobs expects.
