d6d90b490ba46e1795a48ca8ca67d8c7d1594772
Maxwell Austensen <m.austensen@gmail.com>
2022-07-09 21:59:19 -0400

    add copy oca testing data from wow repo

--- tests/test_wowutil.py
+++ tests/test_wowutil.py
@@ -1,10 +1,19 @@
 from unittest import mock
+from pathlib import Path
 import psycopg2
+import shutil
 
 from .conftest import DATABASE_URL
-from load_dataset import Config, load_dataset
+from load_dataset import Config, load_dataset, TEST_DATA_DIR
 import wowutil
 
+MY_DIR = Path(__file__).parent.resolve()
+
+OCA_TEST_DATA_DIR = MY_DIR / "data"
+
+def copy_oca_test_data_to_nycdb_dir():
+    for file in OCA_TEST_DATA_DIR.glob("*oca_*.csv"):
+        shutil.copy(file, TEST_DATA_DIR)
 
 def load_dependee_datasets(config: Config):
     for dataset_name in wowutil.WOW_YML["dependencies"]:
@@ -30,15 +39,16 @@ def test_it_works(db, slack_outbox):
     with mock.patch.dict("os.environ", {"ALGOLIA_API_KEY": ""}, clear=True):
         config = Config(database_url=DATABASE_URL, use_test_data=True)
         load_dependee_datasets(config)
+        copy_oca_test_data_to_nycdb_dir()
 
-        wowutil.main(["build --test"], db_url=DATABASE_URL)
+        wowutil.main(["build", "--test=True"], db_url=DATABASE_URL)
 
         ensure_wow_works()
 
         assert "Rebuilding OCA evictions tables..." in slack_outbox
         assert "Rebuilding Who Owns What tables..." in slack_outbox
         assert "Rebuilding Algolia landlord index..." not in slack_outbox
-        assert slack_outbox[-2] == "Finished rebuilding OCA evictions tables."
+        assert slack_outbox[-3] == "Finished rebuilding OCA evictions tables."
         assert slack_outbox[-1] == "Finished rebuilding Who Owns What tables."
 
         # Ensure that reloading the dependee datasets doesn't raise
@@ -46,6 +56,6 @@ def test_it_works(db, slack_outbox):
         load_dependee_datasets(config)
 
         # Ensure running build again doesn't raise an exception.
-        wowutil.main(["build --test"], db_url=DATABASE_URL)
+        wowutil.main(["build", "--test=True"], db_url=DATABASE_URL)
 
         ensure_wow_works()
