fa96b5718964e5dac8d66af668aa4e2af8146ab0
Maxwell Austensen <m.austensen@gmail.com>
2024-07-09 21:31:16 -0400

    typo

--- tests/test_signatureutil.py
+++ tests/test_signatureutil.py
@@ -52,7 +52,7 @@ def test_it_works(test_db_env, slack_outbox):
         "dob_violations",
         "ecb_violations",
         "dob_complaints",
-        "hpd_vacate_orders",
+        "hpd_vacateorders",
     ]
     for dataset in dependency_datasets:
         subprocess.check_call(["python", "load_dataset.py", dataset], env=test_db_env)
