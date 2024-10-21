2a8354b80442084003dc86b95f722bdc43ef4286
Maxwell Austensen <m.austensen@gmail.com>
2024-10-21 19:25:37 -0400

    fix wow setup before tests

--- tests/test_goodcauseutil.py
+++ tests/test_goodcauseutil.py
@@ -1,8 +1,9 @@
+from unittest import mock
 from pathlib import Path
 import psycopg2
 import subprocess
 
-from tests.test_wowutil import load_dependee_datasets
+from tests.test_wowutil import create_empty_oca_tables, load_dependee_datasets
 from .conftest import DATABASE_URL
 from load_dataset import Config
 import wowutil
@@ -20,30 +21,34 @@ def ensure_goodcause_works():
 
 
 def test_it_works(test_db_env, slack_outbox):
-    # load wow depenedencies (since wow itself is GCE dependency)
-    config = Config(database_url=DATABASE_URL, use_test_data=True)
-    load_dependee_datasets(config)
+    # Let's intentionally disable our access to Algolio
+    # so we don't update the landlord search index
+    with mock.patch.dict("os.environ", {"ALGOLIA_API_KEY": ""}, clear=True):
+        # load wow depenedencies (since wow itself is GCE dependency)
+        config = Config(database_url=DATABASE_URL, use_test_data=True)
+        load_dependee_datasets(config)
+        create_empty_oca_tables()
 
-    # need these additional tables to build good cause eviction data
-    dependency_datasets = [
-        "fc_shd",
-        "nycha_bbls",
-        "hpd_ll44",
-        "dob_certificate_occupancy",
-    ]
-    for dataset in dependency_datasets:
-        subprocess.check_call(["python", "load_dataset.py", dataset], env=test_db_env)
+        # need these additional tables to build good cause eviction data
+        dependency_datasets = [
+            "fc_shd",
+            "nycha_bbls",
+            "hpd_ll44",
+            "dob_certificate_occupancy",
+        ]
+        for dataset in dependency_datasets:
+            subprocess.check_call(["python", "load_dataset.py", dataset], env=test_db_env)
 
-    wowutil.main(["build"], db_url=DATABASE_URL)
+        wowutil.main(["build"], db_url=DATABASE_URL)
 
-    goodcauseutil.main(["build", "--test=True"], db_url=DATABASE_URL)
+        goodcauseutil.main(["build", "--test=True"], db_url=DATABASE_URL)
 
-    ensure_goodcause_works()
+        ensure_goodcause_works()
 
-    assert "Rebuilding Good Cause Eviction tables..." in slack_outbox
-    assert slack_outbox[-1] == "Finished rebuilding Good Cause Eviction tables."
+        assert "Rebuilding Good Cause Eviction tables..." in slack_outbox
+        assert slack_outbox[-1] == "Finished rebuilding Good Cause Eviction tables."
 
-    # Ensure running build again doesn't raise an exception.
-    goodcauseutil.main(["build", "--test=True"], db_url=DATABASE_URL)
+        # Ensure running build again doesn't raise an exception.
+        goodcauseutil.main(["build", "--test=True"], db_url=DATABASE_URL)
 
-    ensure_goodcause_works()
+        ensure_goodcause_works()
